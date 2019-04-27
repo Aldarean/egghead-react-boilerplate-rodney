69fac6064f224de136d4abe52e7a6ff7b59e0637
Rodney Wormsbecher <rodney22@live.nl>
2019-04-27 22:53:06 +0200

    added prettier

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React from 'react'
+import { hot } from 'react-hot-loader'
+
+const Warning = React.lazy(() => import('./Warning'))
 
 class App extends React.Component {
-	render() {
-		return <h1>Hello World</h1>;
-	}
+    state = {
+        count: 0
+    }
+
+    render() {
+        const { count } = this.state
+        return (
+            <div>
+                <h1>Hello World!!! &&&</h1>
+                <h2 className={count > 10 ? 'warning' : null}>
+                    Count: {count}
+                </h2>
+                <button
+                    onClick={() =>
+                        this.setState(state => ({ count: state.count + 1 }))
+                    }
+                >
+                    +
+                </button>
+                <button
+                    onClick={() =>
+                        this.setState(state => ({ count: state.count - 1 }))
+                    }
+                >
+                    -
+                </button>
+                {count > 10 ? (
+                    <React.Suspense fallback={null}>
+                        <Warning />
+                    </React.Suspense>
+                ) : null}
+            </div>
+        )
+    }
 }
 
-export default App;
+export default hot(module)(App)
